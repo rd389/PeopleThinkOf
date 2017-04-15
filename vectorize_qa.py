31793c2cea05a0675270fadb663a68db9dcd1378
soobinhan <sh767@cornell.edu>
2017-04-15 17:38:21 -0400

    repickled

--- vectorize_qa.py
+++ vectorize_qa.py
@@ -7,13 +7,17 @@ handle = open("qa.json")
 j = json.load(handle)
 
 qa_list = [None] * len(j)
+mapping = [None] * len(j)
 for idx, qa in enumerate(j):
     qa_list[idx] = qa["question_text"] + " " + qa["answer_text"]
+    mapping[idx] = (qa["thread_id"], qa["answer_id"])
 
 vectorizer = TfidfVectorizer(min_df = 10, max_df = 0.9)
 tfidf_mat = vectorizer.fit_transform(qa_list)
 
-to_pickle = {'vectorizer': vectorizer, 'matrix': tfidf_mat}
+to_pickle = {'vectorizer': vectorizer,
+             'matrix': tfidf_mat,
+             'mapping': mapping}
 
 with open("qa_vec.pickle", "wb") as handle:
     pickle.dump(to_pickle, handle, protocol = pickle.HIGHEST_PROTOCOL)
