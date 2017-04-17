859ae57b7c5290a64fb383e5f8020275d8ea6625
Jenny Kim <yk494@cornell.edu>
2017-04-16 20:43:35 -0400

    comments for bridging stuff

--- project_template/views.py
+++ project_template/views.py
@@ -8,27 +8,33 @@ from .test import find_similar
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .meta import get_qa_info
+# from .ir import myfunction
 
 # Create your views here.
 def index(request):
-    output_list = ''
-    output=''
-    sample = get_qa_info()
+  output_list = ''
+  output=''
+  sample = get_qa_info()
 
-    if request.GET.get('search'):
-      output = sample
-        # search = request.GET.get('search')
-        # output_list = find_similar(search)
-        # paginator = Paginator(output_list, 10)
-        # page = request.GET.get('page')
-        # try:
-        #     output = paginator.page(page)
-        # except PageNotAnInteger:
-        #     output = paginator.page(1)
-        # except EmptyPage:
-        #     output = paginator.page(paginator.num_pages)
-    return render_to_response('project_template/index.html', 
-                          {'output': output,
-                           'magic_url': request.get_full_path(),
-                           'sample': sample
-                           })
+  if request.GET.get('search'):
+    query = request.GET.get('search')
+    # results = myfunction(query)
+    # output = get_qa_info(results)
+    output = sample
+
+
+    print search
+    # output_list = find_similar(search)
+    # paginator = Paginator(output_list, 10)
+    # page = request.GET.get('page')
+    # try:
+    #     output = paginator.page(page)
+    # except PageNotAnInteger:
+    #     output = paginator.page(1)
+    # except EmptyPage:
+    #     output = paginator.page(paginator.num_pages)
+  return render_to_response('project_template/index.html', 
+                        {'output': output,
+                         'magic_url': request.get_full_path(),
+                         'sample': sample
+                         })
