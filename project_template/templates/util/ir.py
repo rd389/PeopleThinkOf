5eb830ce63e9f5b1ee35ee9c77f6bdb76f69d8c1
Ritwik Dan <rd389@cornell.edu>
2017-04-16 21:46:07 -0400

    cosine similarity search function

--- project_template/templates/util/ir.py
+++ project_template/templates/util/ir.py
@@ -1,2 +1,17 @@
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity as cos_sim
+import numpy as np
+
+def search(query, lim=20):
+    with open('qa_vec.pickle', 'rb') as fp:
+        p = pickle.load(fp)
+    vectorizer = p['vectorizer']
+    mat = p['matrix']
+    mapping = p['mapping']
+    q_vec = vectorizer.transform([query])
+    results = cos_sim(mat, q_vec)
+    rank = np.argmax(results, axis=1)
+    rank = rank[::-1][:lim]
+    results = [mapping[i] for i in rank]
+    return results
