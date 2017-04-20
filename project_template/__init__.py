b9c24873e8b24d2c708fffb59001f341fe32bca4
Jenny Kim <yk494@cornell.edu>
2017-04-19 21:53:55 -0400

    fixed pickle

--- project_template/__init__.py
+++ project_template/__init__.py
@@ -1,6 +1,7 @@
 import pymongo, os, pickle
+from settings import PROJECT_ROOT
 
 MONGO_CLIENT = pymongo.MongoClient(os.environ['MONGODB_URI'])
 DB = MONGO_CLIENT[os.environ['MONGODB_DB_NAME']]
-with open("qa_vec.pickle", "rb") as handle:
+with open(os.path.join(PROJECT_ROOT,"qa_vec.pickle"), "rb") as handle:
     UP_DATA = pickle.load(handle)
