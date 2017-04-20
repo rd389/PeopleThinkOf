001fea3065e6b27fe4b3c41e08d68ef69def6e5e
soobinhan <sh767@cornell.edu>
2017-04-19 21:47:07 -0400

    unpickle once

--- project_template/__init__.py
+++ project_template/__init__.py
@@ -1,4 +1,6 @@
-import pymongo, os
+import pymongo, os, pickle
 
 MONGO_CLIENT = pymongo.MongoClient(os.environ['MONGODB_URI'])
 DB = MONGO_CLIENT[os.environ['MONGODB_DB_NAME']]
+with open("qa_vec.pickle", "rb") as handle:
+    UP_DATA = pickle.load(handle)
