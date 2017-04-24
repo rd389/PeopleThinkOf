e12293b166dc4cb0b603f9604c396b704f8dee34
Jenny Kim <yk494@cornell.edu>
2017-04-24 19:27:27 -0400

    use old search if no descriptor given

--- project_template/views.py
+++ project_template/views.py
@@ -16,15 +16,17 @@ def index(request):
   results_label = ""
 
   if request.GET.get('topic'):
+    topic = request.GET.get('topic')
     desc = request.GET.get('desc')
 
     if desc.strip() == '':
       desc_str = 'all'
+      raw_results = search(topic, lim = 20)
+      
     else:
       desc_str = desc
-
-    topic = request.GET.get('topic')
-    raw_results = categorized_search(topic, desc, lim = 20) #Current lim = 20
+      raw_results = categorized_search(topic, desc, lim = 20) #Current lim = 20
+    
     results = [{"thread_id": res[0], "answer_id": res[1]} for res in raw_results]
     output = get_qa_info(results)
     results_label = 'Showing results for "What do {} people think about {}?" '.format(desc_str, topic)
