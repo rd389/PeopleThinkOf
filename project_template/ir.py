92beac706b40758a35feba81b659116a31ef1092
Charles Yu <charles.yu108@gmail.com>
2017-04-23 21:26:19 -0400

    changes to ir and matrix pickle

--- project_template/ir.py
+++ project_template/ir.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 from settings import PROJECT_ROOT
 from project_template import UP_DATA as p
+from project_template import MATRIX, CAT_LOOKUP
 
 def search(query, lim=20):
     vectorizer = p['vectorizer']
@@ -12,6 +13,22 @@ def search(query, lim=20):
     mapping = p['mapping']
     q_vec = vectorizer.transform([query])
     results = cos_sim(mat, q_vec)
+    rank = np.argsort(results, axis=0)
+    rank = rank[::-1][:lim]
+    results = [mapping[int(i)] for i in rank]
+    return results
+
+
+def categorized_search(query, lim=20, cat):
+	vectorizer = p['vectorizer']
+    mat = p['matrix']
+    mapping = p['mapping']
+    q_vec = vectorizer.transform([query])
+    results = cos_sim(mat, q_vec)
+    #NEED TO IMPORT MATRIX & category lookup
+    cat_vec = MATRIX[CAT_LOOKUP[cat]].T
+    results = np.multiply(results, cat_vec)
+    #may want to define the weighting we will use
     rank = np.argsort(results, axis=0)
     rank = rank[::-1][:lim]
     results = [mapping[int(i)] for i in rank]
