29d267e2d2ea981e334602a63b5eb577a6b27d91
Jenny Kim <yk494@cornell.edu>
2017-04-23 21:56:51 -0400

    using default cats

--- mysite/static/main.js
+++ mysite/static/main.js
@@ -1,19 +1,11 @@
 $( document ).ready(function() {
 
+  var data = {"art": null, "nsfw": null, "politics": null, "modpost": null, "author": null, "travel": null, "actor": null, "crime": null, "philanthropy": null, "other": null, "music": null, "health": null, "gaming": null, null: null, "municipal": null, "business": null, "director": null, "adult": null, "unique": null, "tourism": null, "specialised": null, "customerservice": null, "restaurant": null, "science": null, "medical": null, "request": null, "academic": null, "tech": null, "journalist": null, "military": null, "nonprofit": null, "retail": null, "specialized": null, "athlete": null, "crosspost": null}
+
+  console.log(data)
+
   $('#desc').autocomplete({
-    data: {
-      "fun": null,
-      "rich": null,
-      "stupid": null,
-      "fancy": null,
-      "weird": null,
-      "sassy": null,
-      "sloppy": null,
-      "salty": null,
-      "petty": null,
-      "pretty": null,
-      "sad": null
-    },
+    data: data,
     limit: 6, // The max amount of results that can be shown at once. Default: Infinity.
     onAutocomplete: function (val) {
       // Callback function when value is autcompleted.
