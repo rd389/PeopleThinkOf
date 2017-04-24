f3a5591f735814a695c64eeabb4f81ff938bb0ae
Charles Yu <charles.yu108@gmail.com>
2017-04-23 21:36:48 -0400

    files

--- project_template/__init__.py
+++ project_template/__init__.py
@@ -5,3 +5,9 @@ MONGO_CLIENT = pymongo.MongoClient(os.environ['MONGODB_URI'])
 DB = MONGO_CLIENT[os.environ['MONGODB_DB_NAME']]
 with open(os.path.join(PROJECT_ROOT,"qa_vec.pickle"), "rb") as handle:
     UP_DATA = pickle.load(handle)
+
+#LOAD MATRIX DATA HERE
+with open(os.path.join(PROJECT_ROOT,"default_cats_matrix.pickle"), "rb") as handle:
+    matrix_data= pickle.load(handle)
+    MATRIX = matrix_data["matrix"]
+    CAT_LOOKUP = matrix_data["lookup"]
