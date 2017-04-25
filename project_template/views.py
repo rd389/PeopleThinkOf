1cb008402db866a95f40de187a27b6a1b208627d
Jenny Kim <yk494@cornell.edu>
2017-04-24 20:05:06 -0400

    use empath search + style fixes

--- project_template/views.py
+++ project_template/views.py
@@ -10,11 +10,11 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .meta import get_qa_info
 from .ir import search, categorized_search, search_emp
 
-
 # Create your views here.
 def index(request):
   output=''
   results_label = ""
+  output_header_class = 'hide'
 
   if request.GET.get('topic'):
     topic = request.GET.get('topic')
@@ -23,29 +23,20 @@ def index(request):
     if desc.strip() == '':
       desc_str = 'all'
       raw_results = search(topic, lim = 20)
-      
+
     else:
       desc_str = desc
-      raw_results = categorized_search(topic, desc, lim = 20) #Current lim = 20
-    
+      # raw_results = categorized_search(topic, desc, lim = 20) #Current lim = 20
+      raw_results = search_emp(topic, desc, lim=20)
+
     results = [{"thread_id": res[0], "answer_id": res[1]} for res in raw_results]
     output = get_qa_info(results)
-    results_label = 'Showing results for "What do {} people think about {}?" '.format(desc_str, topic)
+    results_label = 'What do {} people think about {}?'.format(desc_str, topic)
+    output_header_class = 'show'
 
-    # print search
-
-    # output_list = find_similar(search)
-    # paginator = Paginator(output_list, 10)
-    # page = request.GET.get('page')
-    # try:
-    #     output = paginator.page(page)
-    # except PageNotAnInteger:
-    #     output = paginator.page(1)
-    # except EmptyPage:
-    #     output = paginator.page(paginator.num_pages)
-
-  return render_to_response('project_template/index.html',
+  return render_to_response('project_template/index.html', 
                         {'output': output,
                          'magic_url': request.get_full_path(),
                          'result_label': results_label,
+                         'output_header_class': output_header_class
                          })
