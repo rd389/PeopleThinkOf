902d8efd25b8cb6803408a980a5ce80ec04d9079
Jenny Kim <yk494@cornell.edu>
2017-05-01 22:33:28 -0400

    updated autocomplete

--- mysite/static/main.js
+++ mysite/static/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-  var data = {"art": null, "nsfw": null, "politics": null, "author": null, "travel": null, "actor": null, "crime": null, "philanthropy": null, "music": null, "health": null, "gaming": null, "municipal": null, "business": null, "director": null, "adult": null, "unique": null, "tourism": null, "customerservice": null, "restaurant": null, "science": null, "medical": null, "request": null, "academic": null, "tech": null, "journalist": null, "military": null, "nonprofit": null, "retail": null, "specialized": null, "athlete": null}
+  var data = {"art": null, "politics": null, "author": null, "actor": null, "crime": null, "philanthropy": null, "music": null, "health": null, "gaming": null, "government": null, "business": null, "director": null, "adult": null, "unique": null, "tourism": null, "restaurant": null, "science": null, "medical": null, "academic": null, "tech": null, "journalist": null, "military": null, "nonprofit": null, "retail": null, "athlete": null, "food": null}
 
   $('#desc').autocomplete({
     data: data,
